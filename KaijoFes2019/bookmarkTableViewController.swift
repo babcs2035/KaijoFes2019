0d453bef593179a02473ce97c2b6a70f5e35a476
Bwambocos <mijyumaru2525@outlook.jp>
2019-03-12 22:24:47 +0900

    bookmark: RE になる問題を修正，Segue の Identifier 名変更

--- KaijoFes2019/bookmarkTableViewController.swift
+++ KaijoFes2019/bookmarkTableViewController.swift
@@ -8,196 +8,188 @@
 
 import UIKit
 
-class bookmarkTableViewController: UITableViewController {
+class bookmarkTableViewController: UITableViewController
+{
 
     @IBOutlet var testTableView: UITableView!
     
-    //ブックマーク配列
-    var bookmarkList:[String] = []
-    var bookmarkList2:[String] = []
-    
-    //CSVファイルの保存先
-    var userPath:String!
-    
+	
+    var bookmarkList:[String] = []		// ブックマーク配列
+    var userPath:String!				// CSVファイルの保存先
     let fileManager = FileManager()
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+	{
         super.viewDidLoad()
 
         // CSV ファイル読み込み
-        do {
-            //ユーザーが保存したCSVファイルのパス
+        do
+		{
+            // ユーザーが保存した CSV ファイルのパス
             userPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] + "/bookmarks.csv"
             
             var path = userPath
-            if(fileManager.fileExists(atPath: path!) == false){
-                //ユーザーが保存したCSVファイルが無い場合は、初期CSVファイルから読み込む。
+			
+			// ユーザーが保存した CSV ファイルが無い場合
+            if (fileManager.fileExists(atPath: path!) == false)
+			{
+                // 初期 CSV ファイルから読込
                 path = Bundle.main.path(forResource: "bookmarks", ofType: "csv")
-                let csvData2 = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
-                bookmarkList = csvData2.components(separatedBy: "\n")
-                //原因不明のバグを直すための調整
+                let csvData = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+                let temp = csvData.components(separatedBy: "\n")
+				bookmarkList = temp
+                // 原因不明のバグを直すための調整
                 bookmarkList.removeLast()
-            } else {
-                //ユーザーが保存したCSVファイルのデータを取得する。
+            }
+			// ユーザーが保存した CSV ファイルがある場合
+			else
+			{
+                // ユーザーが保存した CSV ファイルのデータを取得
                 let csvData = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
                 bookmarkList = csvData.components(separatedBy: "\n")
             }
             
-            //CSVファイルの出力先を確認する。
+            // CSV ファイルの出力先を確認
             print(userPath)
             
-            //編集ボタン作成
+            // 編集ボタン作成
             navigationItem.rightBarButtonItem = editButtonItem
             
-        } catch {
-            print(error)
-        }
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+		} catch { print(error) }
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+	{
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-    // MARK: - Table view data source
-
-
-    //セル数
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+	
+	// 行数を取得
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
         return bookmarkList.count + 1
     }
 
-    //セル情報
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // セル情報を取得
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
         let cell = tableView.dequeueReusableCell(withIdentifier: "TestCell", for: indexPath)
-
-        
-        
-        if indexPath.row == bookmarkList.count {
-            // 最終行の場合はリセットボタン
-           // cell.textLabel?.textColor = UIColor.red
-            cell.textLabel?.text = "ブックマークをリセットする"
-            cell.detailTextLabel?.text = ""
-            cell.backgroundColor = UIColor.white
-            
-            
-        } else {
-            //カンマでデータを分割して配列に格納する。
-            let bookmarkDetail = bookmarkList[indexPath.row].components(separatedBy: ",")
-            //セルのラベルに企画名、団体名を設定する
-            cell.textLabel?.text = bookmarkDetail[2]
-            cell.detailTextLabel?.text = bookmarkDetail[1]
-            
-            let ampNum = sin(0.3 * Double(indexPath.row)) * 30.0
-            let backColor = UIColor(red: 255/255.0, green: CGFloat((200 + ampNum)/255.0), blue: 15/255.0, alpha: 0.42)
-            cell.backgroundColor = backColor
-            
-        }
-        
-        
-        
+		
+		if (bookmarkList.count > 0)
+		{
+			// 普通の行
+			if (indexPath.row < bookmarkList.count)
+			{
+				// カンマでデータを分割して配列に格納
+				let bookmarkDetail = bookmarkList[indexPath.row].components(separatedBy: ",")
+				
+				// セルのラベルに企画名，団体名を設定
+				cell.textLabel?.text = bookmarkDetail[2]
+				cell.detailTextLabel?.text = bookmarkDetail[1]
+				cell.backgroundColor = UIColor.white
+			}
+			// 最終行
+			else
+			{
+				// リセットボタン
+				cell.textLabel?.text = "ブックマークをリセットする"
+				cell.detailTextLabel?.text = ""
+				cell.textLabel?.textColor = UIColor.red
+				cell.backgroundColor = UIColor.white
+			}
+		}
+		else
+		{
+			cell.textLabel?.text = "何もブックマーク登録されていません"
+			cell.detailTextLabel?.text = ""
+			cell.backgroundColor = UIColor.white
+		}
+		
         return cell
-        
     }
     
-    //編集可能設定
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        if indexPath.row == bookmarkList.count {
-            return false
-        } else {
-            return true
-        }
-        
+    // 編集可能か取得
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+	{
+        return indexPath.row < bookmarkList.count
     }
     
-    //編集モード設定
-    override func setEditing(_ editing: Bool, animated: Bool) {
+    // 編集モードを設定
+    override func setEditing(_ editing: Bool, animated: Bool)
+	{
         super.setEditing(editing, animated: animated)
         tableView.isEditing = editing
     }
-    
-   
-    /*
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        tableView.isEditing = editing
-    }
-    
-    override func tableView(_ tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        return true
-    }
-    */
-    
-    //テーブルビュー編集時に呼ばれるメソッド
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
+	
+	// CSV ファイルに保存
+	func saveToCSV()
+	{
+		
+		// 改行区切りで団体配列を連結
+		let outputStr = bookmarkList.joined(separator: "\n")
+		
+		do
+		{
+			// 団体配列が空の場合
+			if (outputStr == "")
+			{
+				// ユーザーが保存した CSV ファイルを削除
+				try fileManager.removeItem(atPath: userPath)
+			}
+			else
+			{
+				// ファイルを出力
+				try outputStr.write(toFile: userPath, atomically: false, encoding: String.Encoding.utf8)
+			}
+		} catch { print(error) }
+	}
+	
+    // テーブルビューを編集
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+	{
+        if (editingStyle == .delete)
+		{
             bookmarkList.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        
         }
-        //テーブルの再読み込み
+        // テーブルの再読込
         tableView.reloadData()
         
-        //CSVファイルにデータを保存する。
-        saveCSV()
+        // CSV ファイルにデータを保存
+        saveToCSV()
     }
+	
     
-    //CSVファイル保存メソッド
-    func saveCSV() {
-        
-        //改行区切りで部活配列を連結する。
-        let outputStr = bookmarkList.joined(separator: "\n")
-        
-        do {
-            if(outputStr == "") {
-                //部活配列が空の場合はユーザーが保存したCSVファイルを削除する。
-                try fileManager.removeItem(atPath: userPath)
-            } else {
-                //ファイルを出力する。
-                try outputStr.write(toFile: userPath, atomically: false, encoding: String.Encoding.utf8 )
-            }
-        } catch {
-            print(error)
-        }
-    }
-    
-    // Cell が選択された時の処理
+    // Cell 選択時の処理
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
     {
-        if indexPath.row == bookmarkList.count {
-            // セルの選択解除
-            tableView.deselectRow(at: indexPath, animated: true)
-            showAlert()
-            
-        } else {
-            // セルの選択解除
-            tableView.deselectRow(at: indexPath, animated: true)
-            
-            // 指定した ID の Segue を初期化
-            self.performSegue(withIdentifier: "showDetail2", sender: bookmarkList[(indexPath as NSIndexPath).row].components(separatedBy: ","))
-        }
-        
+		tableView.deselectRow(at: indexPath, animated: true)
+		if (bookmarkList.count > 0)
+		{
+			if (indexPath.row == bookmarkList.count)
+			{
+				showAlert()
+			}
+			else
+			{
+				// 指定した ID の Segue を初期化
+				self.performSegue(withIdentifier: "showDetail", sender: bookmarkList[(indexPath as NSIndexPath).row].components(separatedBy: ","))
+			}
+		}
     }
     
-    // セグエで詳細ページに移動する際のデータの受け渡し
+    // Segue で詳細ページに移動する際のデータの受け渡し
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-        // セグエが showDetail の時の処理
-        if segue.identifier == "showDetail2"
+        // Segue が showDetail の時の処理
+        if segue.identifier == "showDetail"
         {
             // 移動先のビューコントローラの data プロパティに値を設定する
             (segue.destination as! eventDetailViewController).data = sender as! [String]
         }
     }
     
-    func showAlert() {
+    func showAlert()
+	{
         
         // アラートを作成
         let alert = UIAlertController(
@@ -205,24 +197,25 @@ class bookmarkTableViewController: UITableViewController {
             message: "本当にブックマークを削除しますか？（この処理は取り消せません）",
             preferredStyle: .alert)
         
-        // アラートにボタンをつける
-        alert.addAction(UIAlertAction(title: "削除", style: .default, handler: { action in
-            self.bookmarkRemove()
-        }))
+        // アラートにボタンを設定
+        alert.addAction(UIAlertAction(title: "削除", style: .default, handler: { action in self.bookmarkRemove()}))
         alert.addAction(UIAlertAction(title: "キャンセル", style: .cancel))
         
-        // アラート表示
+        // アラートを表示
         self.present(alert, animated: true, completion: nil)
     }
     
-    func bookmarkRemove() {
-        if bookmarkList.count != 0 {
+    func bookmarkRemove()
+	{
+        if (bookmarkList.count != 0)
+		{
             bookmarkList.removeAll()
-            //テーブルの再読み込み
+			
+            // テーブルの再読み込み
             tableView.reloadData()
             
-            //CSVファイルにデータを保存する。
-            saveCSV()
+            // CSV ファイルにデータを保存
+            saveToCSV()
         }
     }
 }
