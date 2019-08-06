68bbaad28e4f61f5ea5ba01c5d63b8dba5bb919f
Bwambocos <mijyumaru2525@outlook.jp>
2019-08-06 22:48:39 +0900

    search: eventTable のデザインを大改修

--- KaijoFes2019/searchViewController.swift
+++ KaijoFes2019/searchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SeResultViewController.swift
+//  searchViewController.swift
 //  KaijoFes2019
 //
 //  Created by Bwambocos on 2019/02/01.
@@ -10,16 +10,15 @@ import UIKit
 
 class searchViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate
 {
-    // アウトレット接続
-    @IBOutlet weak var searchBar: UISearchBar!
-    @IBOutlet weak var tableField: UITableView!
-    
-    // 変数
-    var originList:[String] = []		// 全データ
-    var displayList:[String] = []       // 表示するデータ
+	@IBOutlet weak var eventSearchBar: UISearchBar!
+	@IBOutlet weak var eventTable: UITableView!
+	
+	// 変数
+	var originList:[String] = []		// 全データ
+	var displayList:[String] = []       // 表示するデータ
 	var SideBar = sideBarCommon()
 	
-    // 初期化処理
+	// 初期化処理
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -30,116 +29,109 @@ class searchViewController: UIViewController, UITableViewDataSource, UITableView
 		self.view.addSubview(SideBar.sideBarVC.view)
 		SideBar.initSideBar(view: searchViewController())
 		
-		let gradientLayer:CAGradientLayer = CAGradientLayer()
-		let startColor = UIColor(red: 255/255.0, green: 247/255.0, blue: 99/255.0, alpha: 0.8).cgColor
-		let endColor = UIColor(red: 255/255.0, green: 186/255.0, blue: 36/255.0, alpha: 0.8).cgColor
-		gradientLayer.colors = [startColor, endColor]
-		gradientLayer.startPoint = CGPoint(x: 1.0, y: 0.0)
-		gradientLayer.endPoint = CGPoint(x: 0.0, y: 1.0)
-		gradientLayer.frame = view.bounds
-		view.layer.insertSublayer(gradientLayer, at: 0)
-	
-        // デリゲート先の設定
-        searchBar.delegate = self
-        tableField.delegate = self
-        tableField.dataSource = self
-        
-        // 何も入力されていなくても検索キーを押せるようにする
-        searchBar.enablesReturnKeyAutomatically = false
-        
-        // CSV ファイル読み込み
-        do
+		// デリゲート先の設定
+		eventSearchBar.delegate = self
+		eventTable.delegate = self
+		eventTable.dataSource = self
+		
+		// 何も入力されていなくても検索キーを押せるようにする
+		eventSearchBar.enablesReturnKeyAutomatically = false
+		
+		// CSV ファイル読み込み
+		do
 		{
-            // CSV ファイルのデータを取得
-            let csvData = try String(contentsOfFile: Bundle.main.path(forResource: "eventData", ofType: "csv")!, encoding: String.Encoding.utf8)
-            
-            // 改行区切りでデータを分割し，配列に格納
-            originList = csvData.components(separatedBy: "*\n")
-            originList.removeLast()
-        }
+			// CSV ファイルのデータを取得
+			let csvData = try String(contentsOfFile: Bundle.main.path(forResource: "eventData", ofType: "csv")!, encoding: String.Encoding.utf8)
+			
+			// 改行区切りでデータを分割し，配列に格納
+			originList = csvData.components(separatedBy: "*\n")
+			originList.removeLast()
+		}
 		catch { print(error) }
 		
-        // "" を削除
+		// "" を削除
 		for i in 0 ... (originList.count - 1) { originList[i] = originList[i].replacingOccurrences(of: "\"", with: "") }
 		
-        // 表示用配列を初期化
-        displayList = originList
-        
-        // テーブル再表示
-        tableField.reloadData()
+		// 表示用配列を初期化
+		displayList = originList
+		
+		// テーブル再表示
+		eventTable.reloadData()
     }
 	
-    // テーブルの行数を指定する
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
-    {
-        // 表示用配列の要素数 + 1（件数表示行）を返す
-        return displayList.count + 1
-    }
-	
-    // Cell に値を設定する
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	// テーブルのセクション数を設定
+	func numberOfSections(in tableView: UITableView) -> Int
 	{
-        // セルのスタイルを指定する
-        let cell = UITableViewCell(style: UITableViewCell.CellStyle.subtitle, reuseIdentifier: "myCell")
+		return 1
+	}
+	
+	// テーブルの行数を設定
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return displayList.count
+	}
+	
+	// テーブルのセルを設定
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
+		let cell = self.eventTable.dequeueReusableCell(withIdentifier: "eventCell", for: indexPath) as? eventTableViewCell
+		let details = displayList[indexPath.row].components(separatedBy: ",")
+		cell?.eventName.text = details[1]
+		cell?.eventDetail.text = details[4]
+		cell?.eventOrgan.text = details[0]
+		cell?.eventImage.image = UIImage(named: String(details[6]))
+		cell?.eventImage.layer.cornerRadius = 10
+		if details[2] == "expr"
+		{
+			cell?.eventCategory.text = "体験する"
+			cell?.eventCategory.backgroundColor = UIColor.init(red: 20 / 255, green: 255 / 255, blue: 20 / 255, alpha: 1)
+		}
+		if details[2] == "learn"
+		{
+			cell?.eventCategory.text = "学ぶ"
+			cell?.eventCategory.backgroundColor = UIColor.init(red: 20 / 255, green: 20 / 255, blue: 255 / 255, alpha: 1)
+		}
+		if details[2] == "watch"
+		{
+			cell?.eventCategory.text = "観る"
+			cell?.eventCategory.backgroundColor = UIColor.init(red: 255 / 255, green: 20 / 255, blue: 20 / 255, alpha: 1)
+		}
+		if details[2] == "hear"
+		{
+			cell?.eventCategory.text = "聞く"
+			cell?.eventCategory.backgroundColor = UIColor.init(red: 200 / 255, green: 200 / 255, blue: 120 / 255, alpha: 1)
+		}
+		cell?.eventCategory.textColor = UIColor.white
+		cell?.eventCategory.layer.cornerRadius = 5
+		cell?.eventCategory.clipsToBounds = true
+		return cell!
+	}
+	
+	// Cell が選択された時の処理
+	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+	{
+		// セルの選択解除
+		tableView.deselectRow(at: indexPath, animated: true)
 		
-		if indexPath.row == displayList.count
-        {
-            // 最終行の場合は検索結果件数を表示
-            cell.textLabel?.textColor = UIColor.lightGray
-            cell.textLabel?.text = "\(displayList.count) 件の検索結果"
-            
-            // 選択不可にする
-            cell.selectionStyle = .none
-        }
-        else
-        {
-			let details = displayList[indexPath.row].components(separatedBy: ",")
-            cell.textLabel?.text = details[1]
-            cell.detailTextLabel?.text = details[0]
-			
-			// サムネイル用にサークルカットをリサイズ
-			let tempImage:UIImage? = UIImage(named: String(details[6]))
-			var resImage:UIImage
-			if tempImage != nil
-			{
-				resImage = UIImage(named: String(details[6]))!
-			}
-			else
-			{
-				resImage = UIImage(named: "noimage")!
-			}
-			cell.imageView?.image = resImage.resize(size: CGSize.init(width: 50, height: 50))
-        }
+		// 指定した ID の Segue を初期化
+		self.performSegue(withIdentifier: "showDetail", sender: displayList[(indexPath as NSIndexPath).row].components(separatedBy: ","))
+	}
+	
+	// サーチバーで検索ボタンが押された時の処理
+	func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
+	{
+		// キーボードをしまう
+		searchBar.resignFirstResponder()
 		
-        // セルのアクセサリを設定
-        cell.accessoryType = .none
-		
-        return cell
-    }
-    
-    // 最終行を選択不可にする
-    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath?
-    {
-        // 最終行の件数セルは選択不可
-        if indexPath.row >= displayList.count { return nil }
-        else { return indexPath }
-    }
-    
-    // サーチバーで検索ボタンが押された時の処理
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar)
-    {
-        // キーボードをしまう
-        searchBar.resignFirstResponder()
-        
-        if searchBar.text == ""
-        {
-            // 空文字列の場合は全表示
+		if searchBar.text == ""
+		{
+			// 空文字列の場合は全表示
 			displayList = originList
-        }
-        else
-        {
-            // 表示用・伝達用配列を初期化
-            displayList = []
+		}
+		else
+		{
+			// 表示用・伝達用配列を初期化
+			displayList = []
 			
 			for list in originList
 			{
@@ -150,48 +142,38 @@ class searchViewController: UIViewController, UITableViewDataSource, UITableView
 					displayList.append(list)
 				}
 			}
-        }
-        
-        // テーブル再表示
-        tableField.reloadData()
-    }
-    
-    // サーチバーのキャンセルボタンが押された時の処理
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar)
-    {
+		}
+		
+		// テーブル再表示
+		eventTable.reloadData()
+	}
+	
+	// サーチバーのキャンセルボタンが押された時の処理
+	func searchBarCancelButtonClicked(_ searchBar: UISearchBar)
+	{
 		// キーボードをしまう
 		searchBar.resignFirstResponder()
 		
-        // サーチバーの中身を空にする
-        searchBar.text = ""
+		// サーチバーの中身を空にする
+		searchBar.text = ""
 		
-        // 全表示
-        displayList = originList
-        
-        // テーブル再表示
-        tableField.reloadData()
-    }
-    
-    // Cell が選択された時の処理
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
-    {
-        // セルの選択解除
-        tableView.deselectRow(at: indexPath, animated: true)
-        
-        // 指定した ID の Segue を初期化
-        self.performSegue(withIdentifier: "showDetail", sender: displayList[(indexPath as NSIndexPath).row].components(separatedBy: ","))
-    }
-    
-    // セグエで詳細ページに移動する際のデータの受け渡し
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-    {
-        // セグエが showDetail の時の処理
-        if segue.identifier == "showDetail"
-        {
-            // 移動先のビューコントローラの data プロパティに値を設定する
-            (segue.destination as! eventDetailViewController).data = sender as! [String]
-        }
-    }
+		// 全表示
+		displayList = originList
+		
+		// テーブル再表示
+		eventTable.reloadData()
+	}
+
+	// セグエで詳細ページに移動する際のデータの受け渡し
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+	{
+		// セグエが showDetail の時の処理
+		if segue.identifier == "showDetail"
+		{
+			// 移動先のビューコントローラの data プロパティに値を設定する
+			(segue.destination as! eventDetailViewController).data = sender as! [String]
+		}
+	}
 	
 	//　画面遷移時，サイドバーが出ていれば閉じる
 	override func viewWillDisappear(_ animated: Bool)
@@ -202,11 +184,11 @@ class searchViewController: UIViewController, UITableViewDataSource, UITableView
 		}
 	}
 	
-    @IBAction func buttonSearchWord(_ sender: Any)
+	@IBAction func buttonSearchWord(_ sender: Any)
 	{
 		SideBar.performSideBar()
-    }
-    
+	}
+
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
