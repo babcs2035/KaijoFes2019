5daabed83b3c209fddedd90370bbad257a979de0
Bwambocos <mijyumaru2525@outlook.jp>
2019-08-13 20:27:19 +0900

    bookmark: bookmarkTable のデザインを大改修

---
+++ KaijoFes2019/bookmarkTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  bookmarkTableViewCell.swift
+//  KaijoFes2019
+//
+//  Created by Bwambocos on 2019/08/13.
+//  Copyright © 2019 com.babcs2035.kaijofes2019. All rights reserved.
+//
+
+import UIKit
+
+class bookmarkTableViewCell: UITableViewCell
+{
+	@IBOutlet weak var eventImage: UIImageView!
+	@IBOutlet weak var eventName: UILabel!
+	@IBOutlet weak var eventOrgan: UILabel!
+	@IBOutlet weak var eventCategory: UILabel!
+	@IBOutlet weak var eventDetail: UILabel!
+}
